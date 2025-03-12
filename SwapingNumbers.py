7b66d1a8371f78ac87ef0c58df80779b0e8adcab
Maria Raafat <152646754+MariaRaafat@users.noreply.github.com>
2025-03-12 17:15:55 +0200

    Add files via upload

---
+++ SwapingNumbers.py
@@ -0,0 +1,6 @@
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+z=x
+x=y
+y=z
+print("after swapping the first number is {} and the seconde number is {}".format(x,y))
